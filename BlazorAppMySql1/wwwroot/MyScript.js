127f3c753574dddc48b5072a1bb2250f40f8bfc6
danielgontar@gmail.com <danielgontar@gmail.com>
2024-08-25 19:01:51 +0300

    Home Jquery CSS

---
+++ BlazorAppMySql1/wwwroot/MyScript.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+    // Fade in the image and move it slightly upwards
+    $('#university').fadeIn(3000).css('transform', 'translateY(-20px)');
+
+    // Fade in the login form and move it upwards
+    $('.login-form').css('display', 'block'); // Ensure the form is displayed
+    setTimeout(function () {
+        $('.login-form').css({
+            'opacity': 1,
+            'transform': 'translateY(0px)'
+        });
+    }, 2000); // Start the animation after the image has finished fading in
+ });
